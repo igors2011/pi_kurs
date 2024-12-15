3852ebb0f5d783397269c0de3abf647f0c5fd21a
igors <igors-2011@mail.ru>
2024-12-15 18:03:45 +0400

    initializing spring project

---
+++ src/main/java/com/ip/pi_kurs/PiKursApplication.java
@@ -0,0 +1,13 @@
+package com.ip.pi_kurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiKursApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiKursApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ip/pi_kurs/PiKursApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ip.pi_kurs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiKursApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
