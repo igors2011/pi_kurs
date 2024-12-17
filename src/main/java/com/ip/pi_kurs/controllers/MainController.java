27675ee14e7a9b3a0a634c6ee0c17659a8763967
igors <igors-2011@mail.ru>
2024-12-17 16:03:07 +0400

    adding controllers and pages

---
+++ src/main/java/com/ip/pi_kurs/controllers/MainController.java
@@ -0,0 +1,9 @@
+package com.ip.pi_kurs.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/main")
+public class MainController {
+}
